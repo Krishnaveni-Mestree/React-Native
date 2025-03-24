96058be8c2ea4d9b5d41edcad440f0d74ba3f4cd
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-03-24 13:04:36 +0530

    pushing a Assignment4 folder

---
+++ Assignment4/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { 
+    SafeAreaView,
+    ScrollView,
+    TextInput,
+    Pressable,
+    Switch,
+    Text
+ } from "react-native";
+
+ const App=()=>{
+    const [userName,setUserName]=useState('');
+    const [age,setAge]=useState('');
+    const [isOn,setIsOn]=useState(false);
+    return(
+        <SafeAreaView>
+            <Text>Status bar</Text>
+            <ScrollView>
+                <TextInput
+                    style={{borderWidth:1,padding:15}}
+                    value={userName}
+                    placeholder="Enter your name"
+                    onChangeText={(text)=>{
+                        setUserName(text)
+                    }}
+                />
+                <TextInput
+                    style={{borderWidth:1,padding:15}}
+                    value={age}
+                    placeholder="Enter your age"
+                    onChangeText={(val)=>{
+                        setAge(val)
+                    }}
+                />
+                <Pressable
+                    style={{
+                    marginTop: 20,
+                    backgroundColor: 'red',
+                    padding: 20,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    }}
+                    onPress={() => alert('Hi ' + userName + '. You are ' + age + ' years old')}>
+                    <Text>Submit</Text>
+                </Pressable>
+                <Switch
+                    value={isOn}
+                    onValueChange={value => setIsOn(value)}
+                    style={{marginTop: 20}}
+                />
+                {isOn && <Text>{'Hi ' + userName + '. You are ' + age + ' years old'} </Text>}
+            </ScrollView>
+        </SafeAreaView>
+    )
+ }
+
+ export default App;
