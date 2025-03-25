0ab249d0267761bc8f6c8f74d75061716aeac454
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-03-25 17:19:55 +0530

    Infinite scroll for userStories

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { 
   SafeAreaView,
   View,
@@ -14,52 +14,85 @@ import UserStory from "./components/UserStory/UserStory";
 
  const App=()=>{
   const userStories=[
-    {
+    {//id1  //0
       firstName:'Krishna',
       id:1,
       profileImage:require('./assets/images/default_profile.png')
     },
-    {
+    {//id2
       firstName:'Sreekanth',
       id:2,
       profileImage:require('./assets/images/default_profile.png')
     },
-    {
+    {//id3
       firstName:'Vishnu',
       id:3,
       profileImage:require('./assets/images/default_profile.png')
     },
-    {
+    {//id4
       firstName:'Mohan',
       id:4,
       profileImage:require('./assets/images/default_profile.png')
     },
-    {
+    {//id5  //4
       firstName:'shireesha',
       id:5,
       profileImage:require('./assets/images/default_profile.png')
     },
-    {
+    {//id6
       firstName:'Ameen',
       id:6,
       profileImage:require('./assets/images/default_profile.png')
     },
-    {
+    {//id7
       firstName:'Subham',
       id:7,
       profileImage:require('./assets/images/default_profile.png')
     },
-    {
+    {//id8
       firstName:'Kavya',
       id:8,
       profileImage:require('./assets/images/default_profile.png')
     },
-    {
+    {//id9  //8
       firstName:'Shiva',
       id:9,
       profileImage:require('./assets/images/default_profile.png')
     },
-  ]
+  ];
+
+  const userStoriesPageSize=4;
+  const [userStoriesCurrentPage,setUserStoriesCurrentPage]=useState(1);
+  const [userStoriesRenderedData,setUserStoriesRenderedData]=useState([]);
+  const [isLoadingUserStories,setIsLoadingUserStories]=useState(false);
+  //Now what we want to do is create a function that is actually going to populate the data inside the user stories rendered and data.
+  // //For that, we're going to create a function and call it pagination.
+  //What it's going to do is it's going to take the whole database of the users.
+  const pagination=(database,currentPage,pageSize)=>{
+      console.log('current page ',currentPage)
+    //The first time we fetch it, we want it to be index zero.
+    //The second time we fetch it, we want it to be index four.
+    //The third time we fetch it, we want it to be index eight.
+        //Therefore, what we have to come up is a formula of the starting index.
+    const startIndex=(currentPage-1)*pageSize; //0
+    const endIndex=startIndex + pageSize;
+    if(startIndex>=database.length){
+      return [];
+    }
+    return database.slice(startIndex,endIndex);
+  };
+
+
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(() => {
+    setIsLoadingUserStories(true);
+    const getInitialData = pagination(userStories, 1, userStoriesPageSize);
+    setUserStoriesRenderedData(getInitialData);
+    setIsLoadingUserStories(false);
+  }, []);
+  /* eslint-disable react-hooks/exhaustive-deps */  
+
   return(
     <SafeAreaView>
       <Text></Text>
@@ -74,11 +107,26 @@ import UserStory from "./components/UserStory/UserStory";
       </View>
       <View style={globalStyle.userStoryContainer}>
         <FlatList
+          onEndReachedThreshold={0.5}
+          onEndReached={()=>{
+            // console.log('we have reeached the end')
+            if(isLoadingUserStories) {
+              return;
+            }
+            setIsLoadingUserStories(true);
+            const contentToAppend=pagination(userStories,userStoriesCurrentPage+1,userStoriesPageSize);
+            if(contentToAppend.length>0){
+              setUserStoriesCurrentPage(userStoriesCurrentPage+1);
+              setUserStoriesRenderedData(prev=>[...prev, ...contentToAppend]);
+            }
+            setIsLoadingUserStories(false)
+          }}
           showsHorizontalScrollIndicator={false}
           horizontal={true}
-          data={userStories}
+          data={userStoriesRenderedData}
           renderItem={({item})=>(
             <UserStory
+              key={'userStory'+ item.id}
               firstName={item.firstName}
               profileImage={item.profileImage}
             />
