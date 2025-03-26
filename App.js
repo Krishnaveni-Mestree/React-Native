3a3bc8dad6358a47c3217c126ca50296aec158d1
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-03-26 15:13:54 +0530

    Created User Posts by using Flatlist

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import globalStyle from "./assets/styles/globalStyle";
 import UserStory from "./components/UserStory/UserStory";
+import UserPost from "./components/UserPost/UserPost";
 
  const App=()=>{
   const userStories=[
@@ -57,9 +58,69 @@ import UserStory from "./components/UserStory/UserStory";
     {//id9  //8
       firstName:'Shiva',
       id:9,
-      profileImage:require('./assets/images/default_profile.png')
+      profileImage:require('./assets/images/default_profile.png'),
     },
   ];
+
+  //user Posts
+  const userPosts=[
+    {
+      firstName:'Krishnaveni',
+      lastName:'mestree',
+      location:'Hyderabad,India',
+      likes:1201,
+      comments:24,
+      bookmarks:55,
+      image:require('./assets/images/default_post.png'),
+      profileImage:require('./assets/images/default_profile.png'),
+      id:1,
+    },
+    {
+      firstName:'Mohan',
+      lastName:'Rajaa',
+      location:'Chennai,India',
+      likes:1301,
+      comments:25,
+      bookmarks:70,
+      image:require('./assets/images/default_post.png'),
+      profileImage:require('./assets/images/default_profile.png'),
+      id:2,
+    },
+    {
+      firstName:'Shireesha',
+      lastName:'Vanise',
+      location:'vanise,MA',
+      likes:130,
+      comments:8,
+      bookmarks:6,
+      image:require('./assets/images/default_post.png'),
+      profileImage:require('./assets/images/default_profile.png'),
+      id:3,
+    },
+    {
+      firstName:'Ameen',
+      lastName:'Shaik',
+      location:'saudi, UAE',
+      likes:200,
+      comments:47,
+      bookmarks:90,
+      image:require('./assets/images/default_post.png'),
+      profileImage:require('./assets/images/default_profile.png'),
+      id:4,
+    },
+    {
+      firstName:'Subham',
+      lastName:'Subrajith',
+      location:'Daka, BN',
+      likes:302,
+      comments:53,
+      bookmarks:3,
+      image:require('./assets/images/default_post.png'),
+      profileImage:require('./assets/images/default_profile.png'),
+      id:5,
+    },
+];
+
 
   const userStoriesPageSize=4;
   const [userStoriesCurrentPage,setUserStoriesCurrentPage]=useState(1);
@@ -68,6 +129,12 @@ import UserStory from "./components/UserStory/UserStory";
   //Now what we want to do is create a function that is actually going to populate the data inside the user stories rendered and data.
   // //For that, we're going to create a function and call it pagination.
   //What it's going to do is it's going to take the whole database of the users.
+
+  const userPostsPageSize=4;
+  const [userPostsCurrentPage,setUserPostsCurrentPage]=useState(1);
+  const [userPostsRenderedData,setUserPostsRenderedData]=useState([]);
+  const [isLoadingUserPosts,setIsLoadingUserPosts]=useState(false);
+
   const pagination=(database,currentPage,pageSize)=>{
       console.log('current page ',currentPage)
     //The first time we fetch it, we want it to be index zero.
@@ -95,41 +162,63 @@ import UserStory from "./components/UserStory/UserStory";
 
   return(
     <SafeAreaView>
-      <Text></Text>
-      <View style={globalStyle.header}>
-        <Title title={'Lets Explore'}/>
-        <TouchableOpacity style={globalStyle.messageIcon}>
-          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'}/>
-          <View style={globalStyle.messageNumberContainer}>
-            <Text style={globalStyle.messageNumber}>2</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-      <View style={globalStyle.userStoryContainer}>
+      <View>
         <FlatList
-          onEndReachedThreshold={0.5}
-          onEndReached={()=>{
-            // console.log('we have reeached the end')
-            if(isLoadingUserStories) {
-              return;
-            }
-            setIsLoadingUserStories(true);
-            const contentToAppend=pagination(userStories,userStoriesCurrentPage+1,userStoriesPageSize);
-            if(contentToAppend.length>0){
-              setUserStoriesCurrentPage(userStoriesCurrentPage+1);
-              setUserStoriesRenderedData(prev=>[...prev, ...contentToAppend]);
-            }
-            setIsLoadingUserStories(false)
-          }}
-          showsHorizontalScrollIndicator={false}
-          horizontal={true}
-          data={userStoriesRenderedData}
+          ListHeaderComponent={
+          <>
+            <View style={globalStyle.header}>
+              <Title title={'Lets Explore'}/>
+              <TouchableOpacity style={globalStyle.messageIcon}>
+                <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'}/>
+                <View style={globalStyle.messageNumberContainer}>
+                  <Text style={globalStyle.messageNumber}>2</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={globalStyle.userStoryContainer}>
+              <FlatList
+                onEndReachedThreshold={0.5}
+                onEndReached={()=>{
+                  // console.log('we have reeached the end')
+                  if(isLoadingUserStories) {
+                    return;
+                  }
+                  setIsLoadingUserStories(true);
+                  const contentToAppend=pagination(userStories,userStoriesCurrentPage+1,userStoriesPageSize);
+                  if(contentToAppend.length>0){
+                    setUserStoriesCurrentPage(userStoriesCurrentPage+1);
+                    setUserStoriesRenderedData(prev=>[...prev, ...contentToAppend]);
+                  }
+                  setIsLoadingUserStories(false)
+                }}
+                showsHorizontalScrollIndicator={false}
+                horizontal={true}
+                data={userStoriesRenderedData}
+                renderItem={({item})=>(
+                  <UserStory
+                    key={'userStory'+ item.id}
+                    firstName={item.firstName}
+                    profileImage={item.profileImage}
+                  />
+                )}
+              />
+            </View>
+          </>}
+          showsVerticalScrollIndicator={false}
+          data={userPosts}
           renderItem={({item})=>(
-            <UserStory
-              key={'userStory'+ item.id}
-              firstName={item.firstName}
-              profileImage={item.profileImage}
-            />
+            <View style={globalStyle.userPostContainer}>
+              <UserPost
+                firstName={item.firstName}
+                lastName={item.lastName}
+                image={item.image}
+                profileImage={item.profileImage}
+                likes={item.likes}
+                comments={item.comments}
+                bookmarks={item.bookmarks}
+                location={item.location}
+              />
+            </View>
           )}
         />
       </View>
