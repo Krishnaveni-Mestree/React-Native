76462a11cd56c1a94ed7a71ff4c12d76501a5944
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-04-06 17:49:06 +0530

    paginating through categories FlatList

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
 import { Pressable, SafeAreaView, Text, View ,ScrollView, Image } from "react-native";
 
 import Header from "../../components/Header/Header";
@@ -23,11 +23,35 @@ import { updateSelectedCategoryId } from "../../redux/reducers/Categories";
 const Home=()=>{
     const user=useSelector(state=>state.user);
     const dispatch=useDispatch();
-    console.log(user);
+    // console.log(user);
     const categories=useSelector(state=>state.categories);
-    console.log(categories);
-    
-    
+    // console.log(categories);
+    const [categoryPage, setCategoryPage] = useState(1);
+    const [categoryList, setCategoryList] = useState([]);
+    const [isLoadingCategories, setIsLoadingCategories] = useState(false);
+    const categoryPageSize = 4;
+
+    useEffect(() => {
+      setIsLoadingCategories(true);
+      setCategoryList(
+        pagination(categories.categories, categoryPage, categoryPageSize),
+      );
+      setCategoryPage(prev => prev + 1);
+      setIsLoadingCategories(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    console.log(categoryList.length)
+
+    const pagination = (items, pageNumber, pageSize) => {
+      const startIndex = (pageNumber - 1) * pageSize;
+      const endIndex = startIndex + pageSize;
+      if (startIndex >= items.length) {
+        return [];
+      }
+      return items.slice(startIndex, endIndex);
+    };
+
     return(
         <SafeAreaView style={[globalStyle.backGroundWhite,globalStyle.flex]}>
             <Text></Text>
@@ -58,9 +82,30 @@ const Home=()=>{
                 </View>
                 <View style={style.categories}>
                     <FlatList
+                        onEndReachedThreshold={0.5}
+                        onEndReached={() => {
+                          if (isLoadingCategories) {
+                            return;
+                          }
+                          console.log(
+                            'User has reached the end and we are getting more data for page number ',
+                            categoryPage,
+                          );
+                          setIsLoadingCategories(true);
+                          let newData = pagination(
+                            categories.categories,
+                            categoryPage,
+                            categoryPageSize,
+                          );
+                          if (newData.length > 0) {
+                            setCategoryList(prevState => [...prevState, ...newData]);
+                            setCategoryPage(prevState => prevState + 1);
+                          }
+                          setIsLoadingCategories(false);
+                        }}
                         horizontal={true}
                         showsHorizontalScrollIndicator={false}
-                        data={categories.categories}
+                        data={categoryList}
                         renderItem={({item})=>
                             <View
                                 style={style.categoryItem}
