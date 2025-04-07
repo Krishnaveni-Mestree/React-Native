da38b8d231a62bded5acbd3cdbfbebde552f2900
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-04-07 12:31:19 +0530

    Created BackButton component with goBack unction from navigation

--- Screens/SingleDonationItem/SingleDonationItem.js
+++ Screens/SingleDonationItem/SingleDonationItem.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 import style from "./style";
+import { SafeAreaView,ScrollView,Text } from "react-native";
 import { useSelector } from "react-redux";
+import globalStyle from "../../assets/styles/globalStyle";
+import BackButton from "../../components/BackButton/BackButton";
 
-const SingleDonationItem=()=>{
+const SingleDonationItem=({navigation})=>{
     const donationItemInformation=useSelector(state=>state.donations.selectedDonationInformation);
     console.log(donationItemInformation)
-    return null;
+    return (
+        <SafeAreaView style={[globalStyle.backGroundWhite,globalStyle.flex]}>
+            <Text></Text>
+            <ScrollView showsVerticalScrollIndicator={false} style={style.container}>
+                <BackButton onPress={()=>navigation.goBack()}/>
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 export default SingleDonationItem;
--- Screens/SingleDonationItem/style.js
+++ Screens/SingleDonationItem/style.js
@@ -1,7 +1,11 @@
 import { StyleSheet } from "react-native";
+import { horizontalScale, verticalScale } from "../../assets/styles/scaling";
 
 const style=StyleSheet.create({
-
+    container:{
+        marginHorizontal:horizontalScale(20),
+        marginTop:verticalScale(10)
+    }
 });
 
 export default style;
